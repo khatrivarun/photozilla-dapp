2f4fded3080851cf3d8ab81f2516bc7a7790012e
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-10-13 18:03:11 +0530

    Project Setup

--- server/src/modules/dummy/dummy.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DummyController } from './dummy.controller';
-import { DummyService } from './dummy.service';
-
-@Module({
-  controllers: [DummyController],
-  providers: [DummyService],
-})
-export class DummyModule {}
