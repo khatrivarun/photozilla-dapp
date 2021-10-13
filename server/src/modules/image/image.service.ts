b6aa256e10d6dfe5d6866724adc4747be32d7866
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-10-13 19:16:47 +0530

    Route for uploading images implemented. Closes #3

--- server/src/modules/image/image.service.ts
+++ server/src/modules/image/image.service.ts
@@ -1,31 +1,44 @@
 import { PinataPinOptions } from '@pinata/sdk';
 import { PinataService } from './../pinata/pinata.service';
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import * as fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ImageService {
+  private readonly logger: Logger = new Logger(ImageService.name);
   constructor(private readonly pinataService: PinataService) {}
 
   public async uploadToIpfsStorage(
     imageFile: Express.Multer.File,
   ): Promise<string> {
-    const imageName = uuidv4();
-    const readableImageStream = fs.createReadStream(imageFile.path);
+    try {
+      const imageName = uuidv4();
+      const readableImageStream = fs.createReadStream(imageFile.path);
 
-    const options: PinataPinOptions = {
-      pinataMetadata: {
-        name: imageName,
-      },
-      pinataOptions: { cidVersion: 0 },
-    };
+      const options: PinataPinOptions = {
+        pinataMetadata: {
+          name: imageName,
+        },
+        pinataOptions: { cidVersion: 0 },
+      };
 
-    const result = await this.pinataService.pinataClient.pinFileToIPFS(
-      readableImageStream,
-      options,
-    );
+      const result = await this.pinataService.pinataClient.pinFileToIPFS(
+        readableImageStream,
+        options,
+      );
 
-    return result.IpfsHash;
+      return result.IpfsHash;
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException({
+        error: 'Something went wrong, please try again later',
+      });
+    }
   }
 }
